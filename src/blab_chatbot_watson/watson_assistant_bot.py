3b04929709476908ae263dc7ea09a78a4a5990f9
Vinícius <matosvb@gmail.com>
2022-05-16 09:46:00 -0300

    Add missing docstrings

--- src/blab_chatbot_watson/watson_assistant_bot.py
+++ src/blab_chatbot_watson/watson_assistant_bot.py
@@ -1,18 +1,24 @@
-from typing import Any, Protocol, Callable
+"""This module allows the integration of Watson Assistant chatbots with BLAB."""
+from typing import Any, Callable, Protocol
 
-from ibm_watson import AssistantV2
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import AssistantV2
 from ibm_watson.assistant_v2 import MessageInput
 
 
 class Message(Protocol):
-    """Represents a Message (see Message on BLAB Controller's model)"""
+    """Represents a Message (see Message on BLAB Controller's model)."""
 
     type: str
     text: str
     m_id: str
 
     def sent_by_human(self) -> bool:
+        """Check if this message has been sent by a human user.
+
+        Returns:
+            True if and only if the message sender is human
+        """
         pass
 
 
@@ -25,6 +31,8 @@ class ConversationInfo(Protocol):
 
 
 class MessageType:
+    """Represents a message type."""
+
     SYSTEM = 'S'
     TEXT = 'T'
     VOICE = 'V'
@@ -51,12 +59,29 @@ class WatsonAssistantBot:
         api_key: str,
         api_version: str,
         assistant_id: str,
+        dev_environment: bool = False,
     ):
+        """
+        .
+
+        Args:
+            conversation_info: an object that contains
+                information about the conversation (see :cls:`ConversationInfo`)
+            service_url: Watson service URL (shown on Watson website below the API key)
+            api_key: Watson API key (shown on Watson website above service URL)
+            api_version: Watson API version (see the official documentation
+                `here <https://cloud.ibm.com/apidocs/assistant/assistant-v2>`_).
+            assistant_id: id of the assistant (shown on Watson website after
+                launching the chatbot, under "draft environment"/"live environment"
+                - environment settings - API details).
+            dev_environment: whether this is a development environment
+        """
         self.conversation_info = conversation_info
         self.service_url = service_url
         self.api_key = api_key
         self.api_version = api_version
         self.assistant_id = assistant_id
+        self.dev_environment = dev_environment
 
         self.assistant = AssistantV2(
             version=api_version, authenticator=IAMAuthenticator(api_key)
@@ -78,9 +103,21 @@ class WatsonAssistantBot:
         return response['session_id']
 
     def receive_message(self, message: Message) -> None:
+        """Receive a message from the user or other bots.
+
+        Messages from other bots are ignored.
+
+        Args:
+            message: the received message
+        """
         if not message.sent_by_human():
             return
         message_input = MessageInput(message_type='text', text=message.text)
+
+        if self.dev_environment:
+            user_id = 'DEVELOPMENT'
+        else:
+            user_id = 'blab_conv_' + str(self.conversation_info.conversation_id)
 
         response = self.assistant.message(
             assistant_id=self.assistant_id,
@@ -89,7 +126,7 @@ class WatsonAssistantBot:
             ],
             input=message_input,
             context=None,
-            user_id=USER_ID,
+            user_id=user_id,
         ).get_result()
         answers = []
         for r in response['output']['generic']:
@@ -100,8 +137,18 @@ class WatsonAssistantBot:
         for answer in answers:
             self.conversation_info.send_function(answer)
 
-    def process_bot_message(self, message: dict[str, Any]) -> dict[str, Any]:
-        def unify_text(title, description):
+    @classmethod
+    def process_bot_message(cls, message: dict[str, Any]) -> dict[str, Any]:
+        """Process a message received from Watson bot.
+
+        Args:
+            message: the message as received from Watson
+
+        Returns:
+            a dictionary with the message that will be sent back to the user
+        """
+
+        def unify_text(title: str | None, description: str | None) -> str:
             if not description:
                 return title
             if not title:
@@ -111,16 +158,15 @@ class WatsonAssistantBot:
         match (t := message['response_type'].lower()):
             case 'text':
                 return {'type': MessageType.TEXT, 'text': message['text']}
+
             case 'option':
-                # TODO: handle options in a better way
                 return {
                     'type': MessageType.TEXT,
-                    'text': message['title']
-                    + '\n'
-                    + ' / '.join(map(lambda o: o['label'], message['options'])),
+                    'text': message['title'],
+                    'options': list(map(lambda o: o['label'], message['options'])),
                 }
-            case 'image' | 'video' | 'audio':
 
+            case 'image' | 'video' | 'audio':
                 return {
                     'type': {
                         'image': MessageType.IMAGE,
@@ -132,6 +178,6 @@ class WatsonAssistantBot:
                         message.get('title', ''), message.get('description', '')
                     ),
                 }
+
             case _:
-                # TODO: implement other message types
                 return {'type': MessageType.TEXT, 'text': "UNSUPPORTED MESSAGE"}
